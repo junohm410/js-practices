dfa82fe605f2cfc27dfd28cf45c41d786a82571b
junohm410 <junohm410@gmail.com>
2023-09-19 14:03:25 +0900

    fizzbuzz.jsを追加

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+array.forEach((number) => {
+  if (number % 15 === 0) {
+    console.log('FizzBuzz')
+  } else if (number % 5 === 0) {
+    console.log('Buzz')
+  } else if (number % 3 === 0) {
+    console.log('Fizz')
+  } else {
+    console.log(number)
+  }
+})
