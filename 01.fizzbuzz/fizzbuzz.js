3fb7869b8d8868041ab247655556a1d8924c7232
junohm410 <junohm410@gmail.com>
2023-09-19 14:48:11 +0900

    変数名の見直し / prettier

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,15 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 
-array.forEach((number) => {
+numbers.forEach((number) => {
   if (number % 15 === 0) {
-    console.log('FizzBuzz')
+    console.log("FizzBuzz");
   } else if (number % 5 === 0) {
-    console.log('Buzz')
+    console.log("Buzz");
   } else if (number % 3 === 0) {
-    console.log('Fizz')
+    console.log("Fizz");
   } else {
-    console.log(number)
+    console.log(number);
   }
-})
+});
