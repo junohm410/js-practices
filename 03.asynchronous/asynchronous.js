0b07ca88786124589d89ccc66489da330508b0e8
junohm410 <junohm410@gmail.com>
2023-09-25 21:39:13 +0900

    コールバック形式ののsqlite3操作プログラムを追加

--- 03.asynchronous/asynchronous.js
+++ 03.asynchronous/asynchronous.js
@@ -1,9 +1,44 @@
+import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-db.run("create table books(id integer primary key autoincrement, title text unique)",
+db.run(
+  "create table books(id integer primary key autoincrement, title text unique)",
   () =>
     db.run("insert into books (title) values(?)", "チェリー本", function () {
       console.log(`自動採番id: ${this.lastID}`);
+      db.get("select * from books where id = ?", 1, (_, row) => {
+        console.log(`id:${row.id} タイトル:${row.title}`);
+        db.run("drop table books");
+      });
     })
-  )
+);
+
+await timers.setTimeout(100);
+console.log("================================");
+
+db.run(
+  "create table books(id integer primary key autoincrement, title text unique)",
+  () =>
+    db.run("insert into books (title) values(?)", "チェリー本", function () {
+      db.run(
+        "insert into books (title) values(?)",
+        "チェリー本",
+        function (err) {
+          if (err) {
+            console.error(err.message);
+          } else {
+            console.log(`自動採番id: ${this.lastID}`);
+          }
+          db.get("select * from book where id = ?", 1, (err, row) => {
+            if (err) {
+              console.error(err.message);
+            } else {
+              console.error(`id:${row.id} タイトル:${row.title}`);
+            }
+            db.run("drop table books");
+          });
+        }
+      );
+    })
+);
