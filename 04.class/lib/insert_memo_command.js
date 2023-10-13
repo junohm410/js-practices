b0b73bb95d053427045a1d9b0a55d4f6c0827712
junohm410 <junohm410@gmail.com>
2023-10-13 19:20:33 +0900

    readlineモジュールで標準入力を読み取る処理をクラス分け

--- 04.class/lib/insert_memo_command.js
+++ 04.class/lib/insert_memo_command.js
@@ -1,27 +1,14 @@
-import * as readline from "node:readline/promises";
 import { db } from "./memo_app.js";
+import ReadlineInterface from "./readline_interface.js";
 
 export default class InsertMemoCommand {
   execute = async () => {
-    const rl = readline.createInterface({ input: process.stdin });
-    const readInputLines = () => {
-      return new Promise((resolve) => {
-        const lines = [];
-        rl.on("line", (line) => {
-          lines.push(line);
-          console.log(lines);
-        });
-        rl.on("close", () => {
-          resolve(lines);
-        });
-      });
-    };
-    const inputLines = await readInputLines();
-    if (inputLines[0] === "" || inputLines[0].match(/^[\s\u3000]+$/g)) {
+    const readlineInterface = new ReadlineInterface()
+    await readlineInterface.readInputLines();
+    if (readlineInterface.isInputFirstLineEmpty()) {
       console.log("注: 1行目が空白だけのメモは追加できません。");
-      return;
     } else {
-      const newMemo = inputLines.join("\n");
+      const newMemo = readlineInterface.formatInputLinesToMemo();
       db.run(`insert into memos(content) values(?)`, [newMemo], () => {
         console.log("メモの追加が完了しました。");
       });
---
+++ 04.class/lib/readline_interface.js
@@ -0,0 +1,24 @@
+import * as readline from "node:readline/promises";
+
+export default class ReadlineInterface {
+  #inputLines;
+  #interface = readline.createInterface({ input: process.stdin });
+  readInputLines = async () => {
+    this.#inputLines = await new Promise((resolve) => {
+      let lines = [];
+      this.#interface.on("line", (line) => {
+        lines.push(line);
+        console.log(lines);
+      });
+      this.#interface.on("close", () => {
+        resolve(lines);
+      });
+    });
+  };
+  isInputFirstLineEmpty = () => {
+    return this.#inputLines[0] === "" || this.#inputLines[0].match(/^[\s\u3000]+$/g);
+  }
+  formatInputLinesToMemo = () => {
+    return this.#inputLines.join("\n");
+  }
+}
